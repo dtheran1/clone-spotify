f01a6a6ad18abda4bf35cb2bee96188c94fd2b07
dtheran <desarrollador.its15@metgroupsas.com>
2024-01-20 16:16:18 -0500

    wip

---
+++ src/components/Player.jsx
@@ -0,0 +1,84 @@
+// import { usePlayerStore } from "@/store/playerStore";
+import { useEffect, useRef, useState } from "react";
+// import { Slider } from "./Slider";
+
+export const Pause = ({ className }) => (
+  <svg
+    className={className}
+    role="img"
+    height="16"
+    width="16"
+    aria-hidden="true"
+    viewBox="0 0 16 16">
+    <path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path>
+  </svg>
+);
+
+export const Play = ({ className }) => (
+  <svg
+    className={className}
+    role="img"
+    height="16"
+    width="16"
+    aria-hidden="true"
+    viewBox="0 0 16 16">
+    <path d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z"></path>
+  </svg>
+);
+
+export function Player() {
+  // const { currentMusic, isPlaying, setIsPlaying, volume } = usePlayerStore(
+  //   state => state
+  // );
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [currentSong, setCurrentSong] = useState(null);
+  const audioRef = useRef();
+
+  const handleClick = () => {
+    if (isPlaying) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.src = `/music/1/01.mp3`;
+      audioRef.current.play();
+    }
+    setIsPlaying(!isPlaying);
+  };
+
+  // useEffect(() => {
+  //   isPlaying ? audioRef.current.play() : audioRef.current.pause();
+  // }, [isPlaying]);
+
+  // useEffect(() => {
+  //   audioRef.current.volume = volume;
+  // }, [volume]);
+
+  // useEffect(() => {
+  //   const { song, playlist, songs } = currentMusic;
+  //   if (song) {
+  //     const src = `/music/${playlist?.id}/0${song.id}.mp3`;
+  //     audioRef.current.src = src;
+  //     audioRef.current.volume = volume;
+  //     audioRef.current.play();
+  //   }
+  // }, [currentMusic]);
+
+  return (
+    <div className="flex flex-row justify-between w-full px-1 z-50">
+      <div className="w-[200px]"></div>
+
+      <div className="grid place-content-center gap-4 flex-1">
+        <div className="flex justify-center flex-col items-center">
+          <button className="bg-white rounded-full p-2" onClick={handleClick}>
+            {isPlaying ? <Pause /> : <Play />}
+          </button>
+          <SongControl audio={audioRef} />
+          <audio ref={audioRef} />
+        </div>
+      </div>
+
+      <div className="grid place-content-center">
+        <VolumeControl />
+      </div>
+    </div>
+  );
+}
